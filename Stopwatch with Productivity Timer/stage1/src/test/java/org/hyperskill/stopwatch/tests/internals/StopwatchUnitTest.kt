532b538e5a19af252b0ab84b16b384392190619e
RedJocker <mauricio.guaruja@gmail.com>
2022-08-09 21:39:40 -0300

    stage1 - added new version of tests

---
+++ Stopwatch with Productivity Timer/stage1/src/test/java/org/hyperskill/stopwatch/tests/internals/StopwatchUnitTest.kt
@@ -0,0 +1,7 @@
+package org.hyperskill.stopwatch.tests.internals
+
+import android.app.Activity
+import org.hyperskill.projectname.internals.AbstractUnitTest
+
+open class StopwatchUnitTest<T: Activity>(clazz: Class<T>) : AbstractUnitTest<T>(clazz)  {
+}
